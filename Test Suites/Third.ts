28ba2a1b2b7865cd8cd6cb925c287cb2d4aa9884
Logan Aikas <50219221+laikas123@users.noreply.github.com>
2023-07-19 09:11:00 -0700

    test

---
+++ Test Suites/Third.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Third</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>de3fce61-e438-4f8d-a693-148a06b71620</testSuiteGuid>
+</TestSuiteEntity>
