497c8fc84cd012fc55bcd6a291eeae86994c4d66
Logan Aikas <50219221+laikas123@users.noreply.github.com>
2023-07-20 11:01:48 -0700

    maximize window before starting tests

--- Scripts/Test-Div/Script1689875064317.groovy
+++ Scripts/Test-Div/Script1689875064317.groovy
@@ -19,6 +19,8 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
+WebUI.maximizeWindow(FailureHandling.STOP_ON_FAILURE)
+
 WebUI.navigateToUrl('https://v3-test-11.screenpal.com/')
 
 WebUI.click(findTestObject('Object Repository/Page_ScreenPal (Formerly Screencast-O-Matic_a228d3/a_Log In'))
