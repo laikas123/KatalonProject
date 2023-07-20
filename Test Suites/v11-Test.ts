468665db94e5e190ba9a00b51c86128d3ba9afc2
Logan Aikas <50219221+laikas123@users.noreply.github.com>
2023-07-20 11:11:05 -0700

    final v11 test suite

---
+++ Test Suites/v11-Test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>v11-Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e273b1b4-4b7e-4fff-9072-a5c9247a79d0</testSuiteGuid>
+   <testCaseLink>
+      <guid>8ce2c7ef-6a30-4090-a0a7-8c60f742f276</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestDivFinal</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
