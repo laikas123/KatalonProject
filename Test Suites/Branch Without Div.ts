81057b7a6039d09b26811fb3998824341e3acbe0
Logan Aikas <50219221+laikas123@users.noreply.github.com>
2023-07-20 10:58:01 -0700

    added new test suite

---
+++ Test Suites/Branch Without Div.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Branch Without Div</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5e8fe969-1c39-43a3-a71c-28aaf634d9d5</testSuiteGuid>
+   <testCaseLink>
+      <guid>f9222238-fd12-4c5e-9ce8-2e710a9a0a90</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test-Div</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
