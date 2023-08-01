f8187c7db6b9188c2bc035934783480f492b2b09
Logan Aikas <50219221+laikas123@users.noreply.github.com>
2023-08-01 11:47:15 -0700

    test123

---
+++ Object Repository/Page_Plans - Screen Recorder  Video Editor _75d6a2/span_Buy Now.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Buy Now</name>
+   <tag></tag>
+   <elementGuidId>6086b89c-c419-4286-9399-dbeccded0bc6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.module.module-buttons.tb_c6c148.buttons-horizontal.solid.buy-now-premier.circle.buttons-fullwidth > div.module-buttons-item.tf_inline_b > a.ui.builder_button.tb_default_color > span.tf_inline_b.tf_vmiddle</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='themify_builder_content-11005']/div[3]/div/div[2]/div/div/div/div/div/div[5]/div/a/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>db26ad95-ee10-4c6e-bb51-1a7c7e5308ad</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>tf_inline_b tf_vmiddle</value>
+      <webElementGuid>ca5deefb-c2b3-4cd0-9f43-5e0173c1b3c0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Buy Now</value>
+      <webElementGuid>47131c97-71b4-4f34-a62a-abaa7fb1bb52</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;themify_builder_content-11005&quot;)/div[@class=&quot;module_row themify_builder_row repeat fullwidth_row_container tb_70qp45 tf_clearfix&quot;]/div[@class=&quot;row_inner col_align_top gutter-narrow tf_box tf_w tf_rel&quot;]/div[@class=&quot;module_column tb-column col4-1 second tb_unwl47 repeat tf_box&quot;]/div[@class=&quot;tb-column-inner tf_box tf_w&quot;]/div[@class=&quot;module_subrow themify_builder_sub_row repeat tb_6c5w47 tf_w tf_clearfix&quot;]/div[@class=&quot;subrow_inner col_align_top tf_box tf_w&quot;]/div[@class=&quot;sub_column module_column tf_box col-full tb_0ncv47 last&quot;]/div[@class=&quot;tb-column-inner tf_box tf_w&quot;]/div[@class=&quot;module module-buttons tb_c6c148 buttons-horizontal solid buy-now-premier circle buttons-fullwidth&quot;]/div[@class=&quot;module-buttons-item tf_inline_b&quot;]/a[@class=&quot;ui builder_button tb_default_color&quot;]/span[@class=&quot;tf_inline_b tf_vmiddle&quot;]</value>
+      <webElementGuid>ceb4a474-3134-49fd-95d9-be6009622313</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='themify_builder_content-11005']/div[3]/div/div[2]/div/div/div/div/div/div[5]/div/a/span</value>
+      <webElementGuid>ad10c1d4-fcf7-47fc-be14-accaf25584bf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='paid annually'])[2]/following::span[1]</value>
+      <webElementGuid>8c173393-f69c-4d8a-9202-de3c6e5fb5c3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='/mo'])[2]/following::span[1]</value>
+      <webElementGuid>59198759-7db5-45b8-a5fd-351b4840c981</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Deluxe'])[1]/preceding::span[1]</value>
+      <webElementGuid>676e14ad-f60a-4fbb-95d1-6c10c385f6b4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Standard stock media'])[1]/preceding::span[1]</value>
+      <webElementGuid>1e4c2268-57d1-44f6-ba9e-fb9ec0a7cf40</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/div/div[5]/div/a/span</value>
+      <webElementGuid>95829d63-b86b-49a5-a99e-1f06614f5da4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Buy Now' or . = 'Buy Now')]</value>
+      <webElementGuid>40c70db0-5a58-4d15-b18b-75adf7fccd23</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
